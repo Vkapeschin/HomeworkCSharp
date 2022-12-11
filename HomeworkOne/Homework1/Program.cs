37906b98da067fb3021788d72504c1f826d9d85e
Vkapeschin <kapeshin-vladimir@rambler.ru>
2022-12-11 18:37:40 +0300

    Init commit

---
+++ HomeworkOne/Homework1/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача №1. Нахождение большего числа.
+
+/*
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write($"Большим числом является {a}");
+}
+else
+{
+    Console.Write($"Большим числом является {b}");
+}
+*/
+
+//Задача №2. Найти максимальное число. 
+//
+// Не дает присвоить переменной max значение max = a, т.к. у "а" еще нет значения. Как пофиксить - не нашел)
+// Работает с положительными числами.
+//
+/*
+Console.Write("Введите сколько будет чисел для сравнения: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int current = 0;
+int a;
+int max = 0;
+
+while (current < numbers)
+{
+    Console.Write("Введите число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a > max)
+    {
+        max = a;
+    }
+    current++;
+}
+
+Console.WriteLine($"Максимальное число: {max}");
+*/
+
+
+//Задача №2.

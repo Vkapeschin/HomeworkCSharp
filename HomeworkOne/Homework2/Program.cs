c20e088d85f8be0f3071d3acd5b43380606a497e
Vkapeschin <kapeshin-vladimir@rambler.ru>
2022-12-17 23:14:29 +0300

    howework2 added

---
+++ HomeworkOne/Homework2/Program.cs
@@ -0,0 +1,14 @@
+﻿int CutNumber(int num)
+{
+    int sot = num / 100;
+    int ed = num % 10;
+
+    int result = sot * 10 + ed;
+    return result;
+}
+
+int randNum = new Random().Next(100,1000);
+
+int newNum = CutNumber(randNum);
+
+Console.WriteLine($"New version of number {randNum} is {newNum}");

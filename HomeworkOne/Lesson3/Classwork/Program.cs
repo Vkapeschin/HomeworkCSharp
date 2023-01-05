05413101c117f0516c883d911650a6d5a9537b95
Vkapeschin <kapeshin-vladimir@rambler.ru>
2023-01-06 01:36:01 +0300

    Homework3

---
+++ HomeworkOne/Lesson3/Classwork/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+От числа N выдать ряд квадратов до числа N
+
+
+void Method (int n)
+{
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"{i*i} ");
+        }
+}
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Method (num);
+
+*/

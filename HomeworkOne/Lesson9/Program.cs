73a866377572ba74759c415e63ca04530174f80d
Vkapeschin <kapeshin-vladimir@rambler.ru>
2023-02-12 20:02:30 +0300

    exercise 1 done

--- HomeworkOne/Lesson9/Program.cs
+++ HomeworkOne/Lesson9/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿void NumFromNtoM(int n, int m)
+{
+    if (n < m)
+    {
+        Console.Write($"{n} ");
+        NumFromNtoM(n + 1, m);
+    }
+    if (n > m)
+    {
+        Console.Write($"{n} ");
+        NumFromNtoM(n - 1, m);
+    }
+    if (n == m)
+    Console.Write($"{ n}");
+}
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+NumFromNtoM(n, m);
